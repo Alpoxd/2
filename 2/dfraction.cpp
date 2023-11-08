f5437a6b00fc9357ee00b1b9fe74897e7007c79d
alpo <alexanderololoev@gmail.com>
2023-11-08 12:25:48 +0300

    last

--- 2/dfraction.cpp
+++ 2/dfraction.cpp
@@ -3,9 +3,8 @@
 #include "dfraction.h"
 
 void dFraction::print(void) {
+	if (flag == 1) printf("* ");
 	Fraction::print();
-	if (flag == 1) printf("*\n");
-	else printf("\n");
 }
 void dFraction::checkflag() {
 	if (abs(numerator / denominator) >= 10000) flag = 1;
